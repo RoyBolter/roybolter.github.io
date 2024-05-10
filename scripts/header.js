26668939446510a75dbbde17dccfe71fb3e2467a
Roy Bolter <ro465439@ucf.edu>
2024-05-10 15:48:49 -0400

    .

--- scripts/header.js
+++ scripts/header.js
@@ -1,11 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+// <div id="global-header"></div>               // Use this at top of page
+// <script src="scripts/header.js"></script>    // Use this where scripts go
+
+document.addEventListener('DOMContentLoaded', function () {
     const headerHTML = `
         <div class="header">
             <div>
-                <button class="button" onclick="window.location='index.html';">
-                    <img src="../icons/home.png" alt="Home">
-                </button>
+                <button class="button" onclick="window.location='index.html';">Home</button>
                 <button class="button" onclick="window.location='projects.html';">Projects</button>
+                <!--<button class="button" onclick="window.location='games.html';">Games</button>-->
             </div>
             <button class="theme-switcher" onclick="toggleTheme();" id="theme-toggle-btn"></button>
         </div>

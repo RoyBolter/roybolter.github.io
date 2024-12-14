d08295413d82d06556e6dfbf82bf389b772b2571
RoyBolter <ro465439@ucf.edu>
2024-12-14 17:30:04 -0500

    Made Project Buttons Clickable

--- scripts/header.js
+++ scripts/header.js
@@ -6,28 +6,12 @@ document.addEventListener('DOMContentLoaded', function () {
         <div class="header">
             <div>
                 <button class="button" onclick="window.location='index.html';">Home</button>
-                <button class="button" onclick="window.location='projects.html';">Projects</button>
+                <!--<button class="button" onclick="window.location='projects.html';">Projects</button>-->
                 <!--<button class="button" onclick="window.location='games.html';">Games</button>-->
             </div>
-            <button class="theme-switcher" onclick="toggleTheme();" id="theme-toggle-btn"></button>
+            <!--<button class="theme-switcher" onclick="toggleTheme();" id="theme-toggle-btn"></button>-->
         </div>
     `;
 
     document.getElementById('global-header').innerHTML = headerHTML;
-    updateThemeToggleIcon(); // Set the correct theme icon on load
 });
-
-function toggleTheme() {
-    document.body.classList.toggle('light-mode');
-    document.body.classList.toggle('dark-mode');
-    updateThemeToggleIcon();
-}
-
-function updateThemeToggleIcon() {
-    const themeBtn = document.getElementById('theme-toggle-btn');
-    if (document.body.classList.contains('dark-mode')) {
-        themeBtn.style.backgroundImage = "../icons/sun.png";
-    } else {
-        themeBtn.style.backgroundImage = "../icons.moon.png";
-    }
-}
